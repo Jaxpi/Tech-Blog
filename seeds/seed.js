6d136fc68f057691e139c1a5b800ed4ddcaa4aa2
Jax I <jaxpi85@gmail.com>
2022-11-08 16:51:06 -0500

    added sample data to seed files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,13 @@
 const sequelize = require('../config/connection');
 const User = require('../models/users');
 const userData = require('./user-seeds.json');
+const Post = require('./models/posts')
+const postData = require('./post-seeds.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Dish.bulkCreate(dishData, {
+  await Post.bulkCreate(postData, {
     individualHooks: true,
     returning: true,
   });
