1ae3e6411eb56b540d48619887c6d0f7108409a7
Jax I <jaxpi85@gmail.com>
2022-11-08 19:49:45 -0500

    starting readme file

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
+const {User, Post, Comments} = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('home', {
-        loggedIn: true
-    })
-})
+router.get('/', async (req, res) => {
+    try {
+        const content = await Post.findAll({
+            include: [{ 
+              model: User,
+              attributes: ['user_name']
+            }]
+        })
+        res.render('home', {
+        loggedIn: req.session.loggedIn,
+    });
+} catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+}
+});
+
+
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+});
 
 module.exports = router
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/connection');
 const User = require('../models/users');
 const userData = require('./user-seeds.json');
-const Posts = require('./models/posts')
+const Posts = require('../models/posts')
 const postData = require('./post-seeds.json')
 
 const seedDatabase = async () => {
