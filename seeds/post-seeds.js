874ef6dd7271da055d07004777221caef220703c
Jax I <jaxpi85@gmail.com>
2022-11-08 21:12:05 -0500

    added css style sheet and linked to main handlebars but not having an effect

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,8 +1,14 @@
-[
+const { Posts } = require('../models/');
+
+const postData = [
     {
       "post_title": "Sample Title 1",
       "contents": "Sample Contents 1",
       "creator_name": "Sample User Name 1",
       "date_created": "Sample Date 1"
     }
-]
+];
+
+const seedPosts = () => Posts.bulkCreate(postData);
+
+module.exports = seedPosts;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,11 @@
 const sequelize = require('../config/connection');
-const User = require('../models/users');
-const userData = require('./user-seeds.json');
+// const User = require('../models/users');
+// const userData = require('./user-seeds.json');
 const Posts = require('../models/posts')
-const postData = require('./post-seeds.json')
+const postData = require('./post-seeds')
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: false });
+  await sequelize.sync({ force: true });
 
   await Posts.bulkCreate(postData, {
     individualHooks: true,
